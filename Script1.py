4d75c2e6b7083fcc05156cd22dd03d62949fda59
SebastianOlivares11 <seb.olivaresd@duocuc.cl>
2026-04-08 13:05:11 -0400

    Script1.py

---
+++ Script1.py
@@ -0,0 +1,8 @@
+asignatura = "Evaluacion N°1 Programacion y Redes Virtualizadas"
+integrantes = ["Sebastián Olivares"]
+
+print("Información del asignatura")
+print(f"Asignatura: {asignatura}")
+print("Integrantes:")
+for persona in integrantes:
+    print(f"- {persona}")
